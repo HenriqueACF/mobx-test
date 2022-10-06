a574f2ad2dbdc1c84414e942a2eb54c9b9c9f149
henriqueacf <123proteao>
2022-10-06 15:53:59 -0300

    up counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {observer} from 'mobx-react-lite'
 
-function App() {
+const App = observer((props) => {
+  console.log('props ->',props)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Counter</div>
+        <p>Value: {props.store.count}</p>
+        <div className='btns'>
+        <button type='button' onClick={()=>props.store.increment()}>Increment</button>
+        <button type='button' onClick={()=>props.store.decrement()}>Decrement</button>
+        </div>
+        
       </header>
     </div>
   );
-}
+})
 
 export default App;
---
+++ src/Mobx/Store.js
@@ -0,0 +1,21 @@
+import {makeObservable, observable, action} from 'mobx'
+
+class Store{
+  count = 0;
+
+  constructor(value){
+    makeObservable(this, {
+      count: observable,
+      increment: action,
+      decrement: action
+    })
+  }
+  increment(){
+    this.count++
+  }
+  decrement(){
+    this.count--
+  }
+}
+
+export default new Store()
